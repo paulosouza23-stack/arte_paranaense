be4c064a8510ff734a62bea8051a55dfd9dc69c9
paulosouza23-stack <paulo.souza23@escola.pr.gov.br>
2026-09-16 13:47:07 -0300

    salvei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+const botoesCurtir = document.querySelectorALL("curtir");
+botoesCurtir.forEach(function(botoesCurtir){
+let curtiu = false;
+botoesCurtir.addEventListener("click", curtir);
+function curtir(){
+    const contador = botoesCurtir.querySelector("span");
+    if(curtiu === false){
+        contador.textContent++;
+        curtiu = true;}
+        else{
+            contador.textContent--;
+            curtiu = false;
+        }
+}
+});
